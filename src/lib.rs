cbce38c7c9a267fd9f4cbd23748b815018066b2e
Jaimin Pandya <Jaimin.p@alufplastics.com>
2019-02-27 14:44:50 -0500

    Refactored to more rusty code

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-  let mut mut_num = num;
-  let mut digits: Vec<u32> = Vec::new();
-  while mut_num != mut_num % 10 {
-    digits.push(mut_num % 10);
-    mut_num = mut_num / 10;
-  }
-  digits.push(mut_num);
-  let mut sum = 0;
-  for &digit in digits.iter() {
-    sum += u32::pow(digit, digits.len() as u32);
-  }
-  if sum == num {
-    return true;
-  }
-  false
+  let num_str = num.to_string();
+  num_str
+    .chars()
+    .filter_map(|ch| ch.to_digit(10))
+    .map(|digit| digit.pow(num_str.len() as u32))
+    .sum::<u32>()
+    == num
 }
